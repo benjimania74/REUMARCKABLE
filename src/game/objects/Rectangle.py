73b6fd72ff075a0d2136d177842131275d892b79
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-11-15 16:04:35 +0100

    Ajout héritage de Rectangle à RectangleCollider et ajout gestion image et couleur à Rectangle

--- src/game/objects/Rectangle.py
+++ src/game/objects/Rectangle.py
@@ -2,13 +2,14 @@ from pygame import Surface, Color, SRCALPHA
 
 from engine import *
 
+from .image import *
 from game.Utils import calcPercent, toPygameY
 
 class Rectangle(Object):
-    toDraw: Surface
+    toShow: Surface|Image
     drawSurface: Surface
 
-    def __init__(self, x:int|Percent, y:int|Percent, width:int|Percent, height:int|Percent, color:Color, drawSurface: Surface) -> None:
+    def __init__(self, x:int|Percent, y:int|Percent, width:int|Percent, height:int|Percent, texture:Color|str|None, drawSurface: Surface) -> None:
         drawSurfaceWidth: int = drawSurface.get_width()
         drawSurfaceHeight: int = drawSurface.get_height()
 
@@ -18,16 +19,23 @@ class Rectangle(Object):
         self.x = calcPercent(x, drawSurfaceWidth // 2 - self.width // 2, drawSurfaceWidth)
         self.y = calcPercent(y, drawSurfaceHeight // 2 - self.height // 2, drawSurfaceHeight)
 
+        if isinstance(texture, Color) or texture == None:
+            toDraw = Surface((self.width, self.height), flags=SRCALPHA)
+            toDraw.fill(texture if texture != None else Color(0,0,0,0))
+            self.toShow = toDraw
+        else:
+            self.toShow = Image(self.x,self.y,self.width,self.height,texture,drawSurface)
+        
         self.drawSurface = drawSurface
-
-        self.toDraw = Surface((self.width, self.height), flags=SRCALPHA)
-        self.toDraw.fill(color)
-
+    
     def show(self):
-        self.drawSurface.blit(
-            self.toDraw,
-            (
-                self.x,
-                toPygameY(self.y, self.height, self.drawSurface.get_height())
+        if isinstance(self.toShow, Surface):
+            self.drawSurface.blit(
+                self.toShow,
+                (
+                    self.x,
+                    toPygameY(self.y, self.height, self.drawSurface.get_height())
+                )
             )
-        )
+        else:
+            self.toShow.show()
--- src/game/objects/collider/Colliders.py
+++ src/game/objects/collider/Colliders.py
@@ -4,53 +4,19 @@ from engine import Activated
 from game.Utils import toPygameY, calcPercent
 from ..image import Image
 from ..entity import Player
+from ..Rectangle import Rectangle
 
 from pygame import Surface, Color, SRCALPHA
 
-class RectangleCollider(Collideable, Activated):
+class RectangleCollider(Rectangle, Collideable, Activated):
     color: Color|None = None
     image: Image|None = None
     drawSurface: Surface
 
     def __init__(self, x: int|Percent, y: int|Percent, width: int|Percent, height: int|Percent, priority: int, hardColliding:bool, texture: Color|str|None, drawSurface: Surface) -> None:
-        drawSurfaceWidth: int = drawSurface.get_width()
-        drawSurfaceHeight: int = drawSurface.get_width()
-
-        self.width = calcPercent(width, drawSurfaceWidth, drawSurfaceWidth)
-        self.height = calcPercent(height, drawSurfaceHeight, drawSurfaceHeight)
-
-        self.x = calcPercent(x, drawSurfaceWidth // 2 - self.width // 2, drawSurfaceWidth)
-        self.y = calcPercent(y, drawSurfaceHeight // 2 - self.height // 2, drawSurfaceHeight)
-
+        super().__init__(x,y,width,height,texture,drawSurface)
         self.priority = priority
         self.hardColliding = hardColliding
-
-        if texture == None or isinstance(texture, Color):
-            self.color = texture
-        else:
-            self.image = Image(
-                self.x,
-                self.y,
-                self.width,
-                self.height,
-                texture,
-                drawSurface
-            )
-        self.drawSurface = drawSurface
-    
-    def show(self):
-        toDisplay: Surface = Surface((self.width, self.height), SRCALPHA)
-        if self.color != None:
-            toDisplay.fill(self.color)
-            self.drawSurface.blit(
-                toDisplay,
-                (
-                    self.x,
-                    toPygameY(self.y, self.height, self.drawSurface.get_height())
-                )
-            )
-        elif self.image != None:
-            self.image.show()
 
 class ActuatorCollider(RectangleCollider, Actuator):
     def __init__(self, x:int, y:int, w:int, h:int, priority: int, hardColliding: bool, activated: Activated, texture:Color|str|None, surface:Surface) -> None:
