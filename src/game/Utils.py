a59a7bb0bb205bea660fd60a3ecc45697a58e907
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-11-19 10:42:49 +0100

    unification textures

--- src/game/Utils.py
+++ src/game/Utils.py
@@ -2,6 +2,12 @@ from sys import path
 
 from engine.Percent import Percent
 
+from pygame import Color
+
+from typing import Union
+
+Texture = Union[Color,str]
+
 # Constantes
 EXEC_DIRECTORY: str = path[0] + "/"
 
@@ -14,6 +20,17 @@ LEVEL_DIRECTORY: str = EXEC_DIRECTORY + "game/levels/"
 FRAME_NAME: str = "REUMARKABLE"
 FRAME_WIDTH: int = 1280
 FRAME_HEIGHT: int = 720
+
+PLAYER_TEXTURE: Texture = Color(255,255,0)
+PHANTOM_TEXTURE: Texture = Color(0,0,255)
+
+GROUND_TEXTURE: Texture = Color(0,255,0)
+
+DOOR_TEXTURE: Texture = Color(0,255,255)
+
+END_COLLIDER_TEXTURE: Texture = Color(255,0,0)
+
+BUTTON_COLOR: Texture = Color(255,0,255)
 
 # Fonctions utiles
 def toPygameY(y:int, height:int, surfaceHeight:int) -> int:
--- src/game/levels/level1.py
+++ src/game/levels/level1.py
@@ -4,7 +4,7 @@ from typing import Any
 from game.Game import Game
 from game.menu import *
 from game.levels.Level import Level
-from game.Utils import Percent, FRAME_WIDTH, FRAME_HEIGHT
+from game.Utils import *
 from game.menu import *
 from game.objects import *
 
@@ -19,19 +19,19 @@ class Tutorial(Level):
         actuators = self.actuators
 
         rectangleCollidersInfo: list[ tuple[int|Percent,int|Percent,int|Percent,int|Percent,int,bool,Color|str|None] ] = [
-            (0,200,FRAME_WIDTH//2 - 100,50,51,True,Color(0,255,0)), # sol principal
-            (FRAME_WIDTH // 2 - 100, 180, FRAME_WIDTH // 2 + 100, 50, 51, True, Color(0,255,0)), # plateforme basse
-            (Percent(50), 280, FRAME_WIDTH // 4, 50, 51, True, Color(50, 230, 65)), # plateforme haut
+            (0,200,FRAME_WIDTH//2 - 100,50,51,True,GROUND_TEXTURE), # sol principal
+            (FRAME_WIDTH // 2 - 100, 180, FRAME_WIDTH // 2 + 100, 50, 51, True, GROUND_TEXTURE), # plateforme basse
+            (Percent(50), 280, FRAME_WIDTH // 4, 50, 51, True, GROUND_TEXTURE), # plateforme haut
         ]
         for rci in rectangleCollidersInfo:
             colliders.append( RectangleCollider(*rci, game.getScreen()) )
 
-        p: Player = Player(0,400, 50, 50, "background.jpg", colliders, game.getScreen())
-        phantom: PhantomPlayer = PhantomPlayer(0, 350, 50, 50, Color(0,0,255), colliders, game.getScreen())
+        p: Player = Player(0,400, 50, 50, PLAYER_TEXTURE, colliders, game.getScreen())
+        phantom: PhantomPlayer = PhantomPlayer(0, 350, 50, 50, PHANTOM_TEXTURE, colliders, game.getScreen())
         colliders.append(p)
         colliders.append(phantom)
 
-        end: EndGamePlayerDetectorCollider = EndGamePlayerDetectorCollider(1000, 320, 50, 10, 51, True, self, Color(0,255,0), p, game.getScreen())
+        end: EndGamePlayerDetectorCollider = EndGamePlayerDetectorCollider(1000, 320, 50, 10, 51, True, self, END_COLLIDER_TEXTURE, p, game.getScreen())
         colliders.append(end)
 
         toDisplay += colliders
