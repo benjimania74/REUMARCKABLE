2ab470c016b8d20f4269864115fd0a7c865cfe52
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-12-22 16:16:41 +0100

    ajout addition sur Percent

--- src/engine/Percent.py
+++ src/engine/Percent.py
@@ -11,3 +11,8 @@ class Percent:
             return other * self.value / 100
         raise TypeError(f'Cannot multiply Percent with something that isn\'t a number')
     __rmul__ = __mul__
+
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return Percent(self.getValue() + other.getValue())
+        raise TypeError(f'Cannot add Percent with anything except Percent')
